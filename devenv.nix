26d454a4a4305604ccb006f3c6f71a5bd317ae0c
Zev Isert <dev@zevisert.ca>
2024-11-21 08:13:46 -0800

    build: upgrade deps

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,8 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   # https://devenv.sh/languages/
   languages.javascript.enable = true;
-  languages.javascript.package = pkgs.nodejs_18;
+  languages.javascript.package = pkgs.nodejs_22;
   languages.javascript.corepack.enable = true;
-  
+
   # See full reference at https://devenv.sh/reference/options/
 }
