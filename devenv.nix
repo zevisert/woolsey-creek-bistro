dc788f2ea5d80ec47ea7852016fbf392de7b4fad
Zev Isert <dev@zevisert.ca>
2025-03-26 09:08:26 -0700

    chore: update hours
    billing: 5m

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,14 @@
 {pkgs, ...}: {
-  # https://devenv.sh/languages/
-  languages.javascript.enable = true;
-  languages.javascript.package = pkgs.nodejs_22;
-  languages.javascript.corepack.enable = true;
+  packages = [
+    pkgs.keybase
+    pkgs.kbfs
+  ];
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_22;
+    corepack.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
